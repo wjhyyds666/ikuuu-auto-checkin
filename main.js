96d5b568def6afbd10837f8e739afc210e24558a
wjhyyds666 <2742018350@qq.com>
2026-01-04 20:33:15 +0800

    Update main.js

--- main.js
+++ main.js
@@ -28,9 +28,9 @@ async function logIn(account) {
   console.log(`${account.name}: 登录中...`);
 
   const formData = new FormData();
-  formData.append("host", host);
-  formData.append("email", account.email);
-  formData.append("passwd", account.passwd);
+  formData.append("ikuuu.one", host);
+  formData.append("2742018350@qq.com", account.email);
+  formData.append("wjh131400", account.passwd);
   formData.append("code", "");
   formData.append("remember_me", "off");
 
